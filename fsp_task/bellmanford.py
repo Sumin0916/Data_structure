a8189e0c8bcd9f92d17f2a23de40df30df1825b2
Sumin0916 <gsmin2018@gmail.com>
2023-06-03 21:04:27 +0900

    Add bellmanford

--- fsp_task/bellmanford.py
+++ fsp_task/bellmanford.py
@@ -1,15 +1,52 @@
+INF = int(1e9)
+
 class FindShortestPath:
-    def __init__(self):
-        print("Dijkstra")
+    def __init__(self, nodeNum = 7):
+        self.__nodeNum = nodeNum
+        self.__fileData = None
+        self.__adjList = [[] for _ in range(self.__nodeNum+1)]
+        self.__parent = [i for i in range(self.__nodeNum+1)]
+        self.__dist = [INF for _ in range(self.__nodeNum+1)]
 
     def load_data(self, filename):
-        print("load_data ...", filename)
+        try:
+            with open(filename, "r") as f:
+                self.__fileData = f.readlines()
+        except:
+            print("File load fail...")
 
     def initialize(self):
-        print("initialize ...")
+        for line in self.__fileData:
+            start, end, weight = map(int, line.strip().split(','))
+            self.__adjList[start].append([end, weight])
+        self.__dist = [INF for _ in range(self.__nodeNum+1)]
+        self.__parent = [i for i in range(self.__nodeNum+1)]
     
-    def find_path(self, r):
-        print("find_path ...")
+    def find_path(self, start):
+        isCycle = False
+        self.__dist[start] = 0
+        for cycle in range(self.__nodeNum+1):
+            for nowNode in range(self.__nodeNum+1):
+                for nextNode, cost in self.__adjList[nowNode]:
+                    newDist = self.__dist[nowNode]+cost
+                    if (self.__dist[nowNode] != INF) and (newDist < self.__dist[nextNode]):
+                        self.__dist[nextNode] = newDist
+                        self.__parent[nextNode] = nowNode
+                        if cycle == self.__nodeNum:
+                            isCycle = True
+            if isCycle:
+                print("Graph has cycle!!!")
     
     def print_path(self, r):
-        print("print_path ...")
+        for i in range(self.__nodeNum+1):
+            node = i
+            path = [node]
+            while node != 0:
+                node = self.__parent[node]
+                path.append(node)
+            path.reverse()
+            print(f"[ {i} ]: {path[0]}",end="")
+            for n in path[1:]:
+                print(f"=>{n}",end="")
+            print(f" : {self.__dist[i]}")
+        
--- fsp_task/dijkstra.py
+++ fsp_task/dijkstra.py
@@ -1,40 +1,55 @@
 from heapq import heappop,heappush
 
-INF = 987654321
+INF = int(1e9)
 
 class FindShortestPath:
     def __init__(self, nodeNum = 7):
         self.__nodeNum = nodeNum
+        self.__fileData = None
         self.__adjList = [[] for _ in range(self.__nodeNum+1)]
-        self.__shortPaths = [INF for _ in range(self.__nodeNum+1)]
-        self.__queue = []
+        self.__parent = [i for i in range(self.__nodeNum+1)]
+        self.__dist = [INF for _ in range(self.__nodeNum+1)]
 
     def load_data(self, filename):
         try:
-            with open(f"{filename}", "r") as f:
-                lines = f.readlines()
-                for line in lines:
-                    start, end, weight = map(int, line.strip().split(','))
-                    self.__adjList[start].append([end, weight])
+            with open(filename, "r") as f:
+                self.__fileData = f.readlines()
         except:
             print("File load fail...")
 
     def initialize(self):
-        self.__adjList = [[] for _ in range(self.__nodeNum+1)]
-        self.__queue = []
-        self.__shortPaths = [INF for _ in range(self.__nodeNum+1)]    
+        for line in self.__fileData:
+            start, end, weight = map(int, line.strip().split(','))
+            self.__adjList[start].append([end, weight])
+        self.__dist = [INF for _ in range(self.__nodeNum+1)]
+        self.__parent = [i for i in range(self.__nodeNum+1)]
     
     def find_path(self, start):
-        self.__shortPaths[start] = 0
-        heappush(self.__queue, (0, start))
+        queue = []
+        self.__dist[start] = 0
+        heappush(queue, (0, start))
 
-        while self.__queue:
-            pathWeight, nowNode = heappop(self.__queue)
+        while queue:
+            distance, nowNode = heappop(queue)
+            if distance > self.__dist[nowNode]:
+                continue
             for nextNode, cost in self.__adjList[nowNode]:
-                if (pathWeight + cost) < self.__shortPaths[nextNode]: #등호에 따라 결과값 바뀔 수 있음
-                    self.__shortPaths[nextNode] = self.__shortPaths[nowNode] + weight
-                    heappush(self.__adjList, )
-
+                nextDist = distance + cost
+                if nextDist < self.__dist[nextNode]:
+                    self.__parent[nextNode] = nowNode
+                    self.__dist[nextNode] = nextDist
+                    heappush(queue, (nextDist, nextNode))
     
     def print_path(self, r):
-        print("print_path ...")
+        for i in range(self.__nodeNum+1):
+            node = i
+            path = [node]
+            while node != 0:
+                node = self.__parent[node]
+                path.append(node)
+            path.reverse()
+            print(f"[ {i} ]: {path[0]}",end="")
+            for n in path[1:]:
+                print(f"=>{n}",end="")
+            print(f" : {self.__dist[i]}")
+        
